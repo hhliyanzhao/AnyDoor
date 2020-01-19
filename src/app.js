cb938b8c100d9893f76e0b41d77aef66d18d302f
lyz <1660450204@qq.com>
2020-01-19 15:47:44 +0800

    solve multiple callback

--- src/app.js
+++ src/app.js
@@ -1,33 +1,16 @@
 const http = require('http');
 const chalk = require('chalk');
 const path = require('path');
-const fs = require('fs');
+const route = require('./helper/route');
 const config = require('./config/defaultconfig');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req,res) => {
   const url = req.url;
-  const filepath = path.join(config.root, url);
-  fs.stat(filepath, (err, stat) => {
-    if (err) {
-      res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(`${filepath} is not directory or file!`);
-      return;
-    } else if (stat.isFile()) {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      fs.createReadStream(filepath).pipe(res);
-    } else if (stat.isDirectory()) {
-      fs.readdir(filepath, (err, files) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(files.join(','));
-      })
-    }
-  })
+  const filepath = path.join(config.root,url);
+  route(req,res,filepath);
 });
 
-server.listen(config.port, config.hostname, () => {
+server.listen(config.port,config.hostname,() => {
   const addr = `http://${config.hostname}:${config.port}`;
   console.info(`server started at ${chalk.green(addr)}`);
 });
